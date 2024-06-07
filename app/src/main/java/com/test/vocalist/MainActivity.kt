3c3bbc046a09597d168f99f0cf6a6a9505401bae
DESKTOP-2IJ6G78\gilju <giljurha93@gmail.com>
2024-06-07 10:30:23 +0900

    feat: MainActivity RecyclerView 구현

--- app/src/main/java/com/test/vocalist/MainActivity.kt
+++ app/src/main/java/com/test/vocalist/MainActivity.kt
@@ -1,20 +1,50 @@
 package com.test.vocalist
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.test.vocalist.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), WordAdapter.ItemClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var wordAdapter: WordAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initRecyclerView()
+        binding.addButton.setOnClickListener {
+            Intent(this, AddActivity::class.java).let {
+                startActivity(it)
+            }
         }
+    }
+
+    private fun initRecyclerView() {
+        val dummyList = mutableListOf<Word>(
+            Word("weatehr", "날씨", "명사"),
+            Word("honey", "꿀", "명사"),
+            Word("run", "실행하다", "명사"),
+        )
+
+        wordAdapter = WordAdapter(dummyList, this)
+        binding.wordRecyclerView.apply {
+            adapter = wordAdapter
+            layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.VERTICAL, false)
+            val dividerItemDecoration = DividerItemDecoration(applicationContext, LinearLayoutManager.VERTICAL)
+            addItemDecoration(dividerItemDecoration)
+        }
+    }
+
+    override fun onClick(word: Word) {
+        Toast.makeText(this, "${word.text} 가 클릭 됐습니다", Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/test/vocalist/Word.kt
@@ -0,0 +1,7 @@
+package com.test.vocalist
+
+data class Word(
+    val text: String,
+    val mean: String,
+    val type: String,
+)
