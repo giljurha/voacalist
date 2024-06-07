67c257d705242f7f32722dcc67ac20ed9b2a3b36
DESKTOP-2IJ6G78\gilju <giljurha93@gmail.com>
2024-06-07 10:29:43 +0900

    feat: AddActivity Chip 구현

--- app/src/main/java/com/test/vocalist/AddActivity.kt
+++ app/src/main/java/com/test/vocalist/AddActivity.kt
@@ -5,16 +5,35 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.chip.Chip
+import com.test.vocalist.databinding.ActivityAddBinding
 
 class AddActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAddBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_add)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityAddBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() {
+        val types = listOf(
+            "명사", "동사", "대명사", "형용사", "부사", "감탄사", "전치사", "접속사"
+        )
+        binding.typeChipGroup.apply {
+            types.forEach { text ->
+                addView(createChip(text))
+            }
+        }
+    }
+
+    private fun createChip(text: String): Chip {
+        return Chip(this).apply {
+            setText(text)
+            isCheckable = true
+            isCheckable = true
         }
     }
 }
